472805164933e0ff8e206fd9c0d5a96a3bc400d8
Vitor <vitordearec@gmail.com>
2021-09-17 17:54:36 -0300

    Criando a arquivo para a tela de formulario de login

---
+++ lib/frm_login.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class FrmLgn extends StatefulWidget {
+  const FrmLgn({Key? key}) : super(key: key);
+
+  @override
+  _FrmLgnState createState() => _FrmLgnState();
+}
+
+class _FrmLgnState extends State<FrmLgn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
