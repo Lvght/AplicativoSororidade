27afe94417d9ccf49ecf27424248751d43938110
Vitor <vitordearec@gmail.com>
2021-09-21 20:53:58 -0300

    Tela com imagem e espaços de texto para usuario e senha. Tendo validaçao no usuario.

--- lib/frm_login.dart
+++ lib/frm_login.dart
@@ -9,48 +9,68 @@ class FrmLgn extends StatefulWidget {
 
 class _FrmLgnState extends State<FrmLgn> {
   String dirLogo = "assets/images/Logo_EntreElas.jpeg";
-  TextEditingController nomeController = TextEditingController();
+  List<String> users = ["Leila0102", "Joana00"];
 
-  Widget creatorTextFild(){
-    return Expanded(
-      child: TextField(
-        decoration: const InputDecoration(
-          hintText: 'Insira seu nome',
-      ),
-      )
-    );
-  }
-  
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Container(
-            child: Image.asset(dirLogo), 
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(92))
-            //TextFiel()
-          ),
-          
-        
-          //creatorTextFild(),
-
-          /*TextField(
-            //decoration: const InputDecoration(
-              //hintText: 'Insira seu nome',
-            //),
-          )*/
-        ],
-      ),
-    );
-  }
-/*
-Container(
         color: Colors.white,
-        child: TextField(
-            decoration: const InputDecoration(
-          hintText: "Insira seu nome",
-*/
-  
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 60, 20, 0),
+          child: Column(
+            children: [
+              Container(
+                  height: 184,
+                  width: 184,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(dirLogo),
+                        fit: BoxFit.fitHeight,
+                      ),
+                      borderRadius: BorderRadius.circular(92),
+                      border: Border.all(
+                          color: const Color(0xFF2AADE3),
+                          style: BorderStyle.solid,
+                          width: 3))),
+              // Form de usuária com validação de usuária.
+              Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Material(
+                      color: Colors.transparent,
+                      child: TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (user) {
+                          if (user != null && users.contains(user)) {
+                            return ("Este nome de usuária já está em uso. Digite um novo.");
+                          }
+                          return null;
+                        },
+                        style: const TextStyle(
+                            fontFamily: "Montserrat", fontSize: 15),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            hintText: "Digite seu nome de usuária"),
+                      ))),
+              // Form de senha.
+              Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Material(
+                      color: Colors.transparent,
+                      child: TextField(
+                        style: const TextStyle(
+                            fontFamily: "Montserrat", fontSize: 15),
+                        obscureText: true,
+                        obscuringCharacter: "*",
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            hintText: "Senha"),
+                      )))
+            ],
+          ),
+        ));
+  }
 }
